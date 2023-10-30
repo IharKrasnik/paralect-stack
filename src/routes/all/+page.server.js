33fdf26dc6431e4f77bca520de4265ead0df14cf
Igor Krasnik <ihar.krasnik@gmail.com>
2023-10-30 17:10:29 +0100

    All meta

---
+++ src/routes/all/+page.server.js
@@ -0,0 +1,7 @@
+export async function load({ params }) {
+	return {
+		ogTitle: 'All Startup Stacks — Paralect Stack',
+		ogDescription:
+			'Discover new tech startup tools and learn how other founders build their startups. Submit your stack!'
+	};
+}
